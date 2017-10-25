cfbff9deecbf1c5e8fc334674329085fe58716d1
tszhonglau <lauxx265@morris.umn.edu>
2017-10-25 00:37:29 -0500

    Able to print database names

---
+++ node/summarize-db.js
@@ -0,0 +1,42 @@
+var async=require('async');
+
+var credentials = require('./credentials.json');
+
+var mysql=require("mysql");
+
+credentials.host="ids";
+var connection = mysql.createConnection(credentials);
+
+var users = [];
+var databaseNames = [];
+async.series([
+	function(callback) {
+		connection.connect(function(err){
+			if(err){
+				console.log("Problems with MySQL: "+err);
+			} else {
+				console.log("Connected to Database.");
+			}
+		});
+		
+		connection.query('SHOW DATABASES', function(err, rows, fields){
+			if(err){
+				console.log('Error looking up databases');
+			} else{
+				for(var i in rows) {
+					databaseNames[i] = rows[i].Database;
+				 	console.log(databaseNames[i]);	
+				}
+			}
+		});
+		callback();
+		
+	},
+	function(callback) {
+		connection.end()
+		console.log("All done now.");
+		callback();
+	}
+
+]);
+
