b830064bea233aa91108653234bb40baa729d842
tszhonglau <lauxx265@morris.umn.edu>
2017-10-27 07:24:43 -0500

    Displaying the fieldName and its data type correctly, as well as the corresponding tableName, however, it does not display databaseName in the order it should be

--- node/summarize-db.js
+++ node/summarize-db.js
@@ -1,7 +1,7 @@
 var credentials = require('./credentials.json');
 
 var mysql=require("mysql");
-var count = 0;
+var bool = false;
 credentials.host="ids";
 var connection = mysql.createConnection(credentials);
 
@@ -17,48 +17,51 @@ connection.query('SHOW DATABASES',function(err,rows,fields){
   if(err){
     console.log('Error looking up databases');
   } else {
-    
     for(var j in rows) {
-		  getTables(rows[j].Database, j, rows.length);
-		}
+	console.log("---|" + rows[j].Database + ">");
+	getTables(rows[j].Database, j, rows.length);	  	
+    }
+   	  
 		
 }
 });
 
 
 function getDescription(dB, tableName, index, length, indexTable, lengthTable){
-  connection.query('DESCRIBE ' + dB + '.' + tableName, function(err, rows, fields){
+  connection.query('DESCRIBE ' + dB + '.' + tableName, (function(tableName) {return function(err, rows, fields){
     if(err){
       console.log('errors');
     } 
     else{
-      console.log('this is index' + index + " " + indexTable);
-      if(index == (length - 2) && indexTable == (lengthTable - 1)) {
-        connection.end();
-	return;
-      }
+      console.log(".....|" + dB + "." + tableName + ">");
+      for(var j in rows) {      
+      	console.log("        FieldName: " + "`" + rows[j].Field + "`" +  "     (" + rows[j].Type + ")");
+      	if(index == (length - 2) && indexTable == (lengthTable - 1)) {
+        	connection.end();
+		return;
+      	}
+     }
     }
+	
     
-  });
+  }})(tableName));
   
 }
 
 function getTables(name, index, length){
-  connection.query('SHOW TABLES FROM ' + name, function(err, tables, fields){
+  connection.query('SHOW TABLES FROM ' + name, (function(name) {return function(err, tables, fields){
   if(err){
     console.log('error tables');
-  } 
-    else{
-    for(var k in tables) {
-				var tmpStr2 = "Tables_in_"+ name;
-				getDescription(name, tables[k][tmpStr2], index, length, k, tables.length);
-			}
+  } else{
+       console.log("---|" + name + ">");
+       for(var k in tables) {
+	var tmpStr2 = "Tables_in_"+ name;
+	getDescription(name, tables[k][tmpStr2], index, length, k, tables.length);
+    }
+   
   }
   
   
-});
+  }})(name));
 }
 
-  
-
-console.log("All done now.");
