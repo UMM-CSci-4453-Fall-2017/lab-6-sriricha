6141ffa2e43a42106e1289560d9c54d97239a903
tszhonglau <lauxx265@morris.umn.edu>
2017-10-27 07:40:35 -0500

    Finished summarize-db, output match on readme

--- node/summarize-db.js
+++ node/summarize-db.js
@@ -1,7 +1,7 @@
 var credentials = require('./credentials.json');
 
 var mysql=require("mysql");
-var bool = false;
+var str = "";
 credentials.host="ids";
 var connection = mysql.createConnection(credentials);
 
@@ -10,6 +10,7 @@ connection.connect(function(err){
     console.log("Problems with MySQL: "+err);
   } else {
     console.log("Connected to Database.");
+    console.log("      Acquiring data. This may take a bit...");
   }
 });
 
@@ -18,22 +19,23 @@ connection.query('SHOW DATABASES',function(err,rows,fields){
     console.log('Error looking up databases');
   } else {
     for(var j in rows) {
-	console.log("---|" + rows[j].Database + ">");
 	getTables(rows[j].Database, j, rows.length);	  	
-    }
-   	  
-		
+    }		
 }
 });
 
 
 function getDescription(dB, tableName, index, length, indexTable, lengthTable){
-  connection.query('DESCRIBE ' + dB + '.' + tableName, (function(tableName) {return function(err, rows, fields){
+  connection.query('DESCRIBE ' + dB + '.' + tableName, function(err, rows, fields){
     if(err){
       console.log('errors');
     } 
     else{
-      console.log(".....|" + dB + "." + tableName + ">");
+      if (dB != str) {
+        console.log("---|" + dB + ">");
+	str = dB;      
+      }	    
+      console.log("......|" + dB + "." + tableName + ">");
       for(var j in rows) {      
       	console.log("        FieldName: " + "`" + rows[j].Field + "`" +  "     (" + rows[j].Type + ")");
       	if(index == (length - 2) && indexTable == (lengthTable - 1)) {
@@ -41,27 +43,22 @@ function getDescription(dB, tableName, index, length, indexTable, lengthTable){
 		return;
       	}
      }
-    }
-	
-    
-  }})(tableName));
+    }  
+  });
   
 }
 
 function getTables(name, index, length){
-  connection.query('SHOW TABLES FROM ' + name, (function(name) {return function(err, tables, fields){
+  connection.query('SHOW TABLES FROM ' + name, function(err, tables, fields){
   if(err){
     console.log('error tables');
   } else{
-       console.log("---|" + name + ">");
        for(var k in tables) {
 	var tmpStr2 = "Tables_in_"+ name;
 	getDescription(name, tables[k][tmpStr2], index, length, k, tables.length);
     }
    
   }
-  
-  
-  }})(name));
+  });
 }
 
