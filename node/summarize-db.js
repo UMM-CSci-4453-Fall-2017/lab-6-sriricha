e7759392ae821914f2385248f327bf1bbff2a8e3
tszhonglau <lauxx265@morris.umn.edu>
2017-10-29 19:08:36 -0500

    Added connection.end() in case error is encountered during query

--- node/summarize-db.js
+++ node/summarize-db.js
@@ -8,6 +8,7 @@ var connection = mysql.createConnection(credentials);
 connection.connect(function(err){
   if(err){
     console.log("Problems with MySQL: "+err);
+    connection.end();
   } else {
     console.log("Connected to Database.");
     console.log("      Acquiring data. This may take a bit...");
@@ -17,6 +18,7 @@ connection.connect(function(err){
 connection.query('SHOW DATABASES',function(err,rows,fields){
   if(err){
     console.log('Error looking up databases');
+    connection.end();
   } else {
     for(var j in rows) {
 	getTables(rows[j].Database, j, rows.length);	  	
@@ -29,6 +31,7 @@ function getDescription(dB, tableName, index, length, indexTable, lengthTable){
   connection.query('DESCRIBE ' + dB + '.' + tableName, function(err, rows, fields){
     if(err){
       console.log('errors');
+      connection.end();
     } 
     else{
       if (dB != str) {
@@ -52,6 +55,7 @@ function getTables(name, index, length){
   connection.query('SHOW TABLES FROM ' + name, function(err, tables, fields){
   if(err){
     console.log('error tables');
+    connection.end();
   } else{
        for(var k in tables) {
 	var tmpStr2 = "Tables_in_"+ name;
